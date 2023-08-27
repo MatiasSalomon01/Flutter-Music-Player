e87fa4824c75a8ac788be9e0411492152d483fdd
=Matias Salomon <matias.salomon@clt.com.py>
2023-08-26 22:40:01 -0400

    cambios varios

--- lib/widgets/custom_listtile.dart
+++ lib/widgets/custom_listtile.dart
@@ -8,7 +8,7 @@ import 'package:music_player/services/song_service.dart';
 import 'package:music_player/widgets/widgets.dart';
 import 'package:provider/provider.dart';
 
-class CustomListTile extends StatelessWidget {
+class CustomListTile extends StatefulWidget {
   final SongModel data;
   final int index;
   final int length;
@@ -20,12 +20,18 @@ class CustomListTile extends StatelessWidget {
   });
 
   @override
+  State<CustomListTile> createState() => _CustomListTileState();
+}
+
+class _CustomListTileState extends State<CustomListTile> {
+  bool isActive = false;
+  @override
   Widget build(BuildContext context) {
     final songService = Provider.of<SongService>(context);
 
     return Padding(
       padding: EdgeInsets.only(
-          bottom: index == (length - 1)
+          bottom: widget.index == (widget.length - 1)
               ? songService.currentSong.id!.isNotEmpty
                   ? 140
                   : 80
@@ -36,17 +42,21 @@ class CustomListTile extends StatelessWidget {
           var audioProvider =
               Provider.of<AudioProvider>(context, listen: false);
 
-          songService.currentIndex = index;
-          if (data.id == songService.currentSong.id) return;
-          songService.currentSong = data;
+          songService.currentIndex = widget.index;
+          if (widget.data.id == songService.currentSong.id) return;
+
+          songService.currentSong = widget.data;
           audioProvider.setUrl = songService.currentSong.url;
           audioProvider.play();
           songService.setCurrentSong();
+          setState(() {
+            isActive = !isActive;
+          });
         },
         leading: ClipRRect(
           borderRadius: BorderRadius.circular(5),
           child: Image.network(
-            data.albumImage,
+            widget.data.albumImage,
             fit: BoxFit.cover,
             loadingBuilder: (context, child, loadingProgress) =>
                 Container(color: darkGrey, width: 60, height: 60, child: child),
@@ -56,14 +66,20 @@ class CustomListTile extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(data.title, style: const TextStyle(color: white)),
-            Text(data.artists, style: TextStyle(color: Colors.grey.shade600)),
+            Text(widget.data.title,
+                style: TextStyle(
+                    color: widget.data.id == songService.currentSong.id
+                        ? lightGreen
+                        : white)),
+            Text(widget.data.artists,
+                style: TextStyle(color: Colors.grey.shade600)),
           ],
         ),
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            CustomAnimatedIcon(isFavorite: data.isFavorite, id: data.id!),
+            CustomAnimatedIcon(
+                isFavorite: widget.data.isFavorite, id: widget.data.id!),
             IconButton(
               onPressed: () {},
               icon: Icon(
