0824f69ac69206a3a6c4a731c7167ac5d450eecc
=Matias Salomon <matias.salomon@clt.com.py>
2023-09-08 23:13:23 -0400

    feat: showPlaylistModal para el CustomListTile + funcionalidad para opcion de me gusta funcional

--- lib/widgets/custom_listtile.dart
+++ lib/widgets/custom_listtile.dart
@@ -1,13 +1,13 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:music_player/constants/constants.dart';
 import 'package:music_player/models/models.dart';
 import 'package:music_player/providers/providers.dart';
-import 'package:music_player/screens/screens.dart';
 import 'package:music_player/services/song_service.dart';
 import 'package:music_player/utlis/utils.dart';
 import 'package:music_player/widgets/widgets.dart';
 import 'package:provider/provider.dart';
+
+import '../models/playlists.dart';
 
 class CustomListTile extends StatefulWidget {
   final SongModel data;
@@ -52,6 +52,7 @@ class _CustomListTileState extends State<CustomListTile> {
           songService.setCurrentSong();
           setState(() => isActive = !isActive);
         },
+        onLongPress: () => showModal(context, widget.data),
         leading: ClipRRect(
           borderRadius: BorderRadius.circular(5),
           child: Image.network(
@@ -84,7 +85,7 @@ class _CustomListTileState extends State<CustomListTile> {
             CustomAnimatedIcon(
                 isFavorite: widget.data.isFavorite, id: widget.data.id!),
             IconButton(
-              onPressed: () => showPlaylistModal(context),
+              onPressed: () => showModal(context, widget.data),
               icon: Icon(
                 Icons.more_vert,
                 color: Colors.grey.shade600,
@@ -95,4 +96,156 @@ class _CustomListTileState extends State<CustomListTile> {
       ),
     );
   }
+
+  void showModal(BuildContext context, SongModel song) => showPlaylistModal(
+        context,
+        height: 310,
+        header: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            children: [
+              Container(
+                width: 55,
+                height: 55,
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                child: Image.network(
+                  song.albumImage,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      Container(
+                    color: darkGrey,
+                    width: 55,
+                    height: 55,
+                    child: child,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .75,
+                child: Text(
+                  song.title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: white,
+                    fontSize: 20,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+              leading: Icon(
+                song.isFavorite ? Icons.favorite : Icons.favorite_border,
+                size: 30,
+                color: song.isFavorite ? Colors.red : Colors.grey,
+              ),
+              title: Text(
+                song.isFavorite ? 'Diste me gusta' : 'Me gusta',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: white,
+                  fontSize: 20,
+                ),
+              ),
+              onTap: () async {
+                //TODO: refactorizar este metodo
+                final songService =
+                    Provider.of<SongService>(context, listen: false);
+
+                await songService.updateById(song.id!, !song.isFavorite);
+                Navigator.pop(context);
+                var x = songService.songs
+                    .firstWhere((element) => element.id == song.id);
+
+                if (songService.playlists.isNotEmpty) {
+                  var likedSongs = songService.playlists.first;
+                  var exists =
+                      likedSongs.songs.any((element) => element.id == song.id);
+                  if (exists == false && song.isFavorite == true) {
+                    likedSongs.songs.add(x);
+                    await songService.updateLikedSong(likedSongs);
+                  }
+                  if (exists == true && song.isFavorite == false) {
+                    likedSongs.songs
+                        .removeWhere((element) => element.id == song.id);
+                    await songService.updateLikedSong(likedSongs);
+                  }
+                } else {
+                  var playlist = Playlist(
+                    title: 'Tus me gusta',
+                    totalSongs: 1,
+                    image:
+                        'https://firebasestorage.googleapis.com/v0/b/flutter-music-player-9518c.appspot.com/o/images%2Fliked-songs-300.png?alt=media&token=b89872ec-3c82-4317-831e-651b84606206',
+                    albumCover:
+                        'https://firebasestorage.googleapis.com/v0/b/flutter-music-player-9518c.appspot.com/o/images%2Falbum-default.png?alt=media&token=e6c6d08e-68dc-4fd4-bcf0-d153f2f1ad25',
+                    songs: [x],
+                    isPinned: false,
+                  );
+                  songService.playlists = [playlist];
+                  await songService.updateLikedSong(playlist);
+                }
+              },
+            ),
+            ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+              leading: SizedBox(
+                width: 40,
+                child: Stack(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(left: 6),
+                      child: Icon(
+                        Icons.music_note_outlined,
+                        size: 30,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Positioned(
+                      top: -3,
+                      left: -4,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 3),
+                        child: Icon(
+                          Icons.add,
+                          size: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              title: const Text(
+                'Agregar a playlist',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            const ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 10),
+              leading: Icon(
+                Icons.share_outlined,
+                size: 30,
+                color: Colors.grey,
+              ),
+              title: Text(
+                'Compartir',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
 }
